5d1afe1807dbaefeed382c28b37add1e41eba27b
Jordan McDonald <Jmacthetaii@gmail.com>
2022-03-30 21:11:44 +1100

    Working user system, it's a fucking nightmare.

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,16 @@
-#include <User.h>
-
 // Included libararies
 #include <SPI.h>     // Required for RFID Reader
 #include <MFRC522.h> // Required for RFID Reader
 #include <Keypad.h>  // Required for Keypad
-
+#include <User.h>    // My own little library!
 
 // General initialisation
 
+#define TOTAL_USERS 2
 bool phase1Verified = false;
 bool phase2Verified = false;
+int current_user;
+String input_password;
 
 // Keypad initialisation
 const int ROW_NUM = 4;    // four rows
@@ -26,52 +27,70 @@ byte pin_column[COLUMN_NUM] = {31, 33, 35, 37}; // connect to the column pinouts
 
 Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
 
-String input_password;
+// The list of whitelisted users. Each user has a User ID, a Password, and a Card ID.
+// The User ID is used only within the system to differentiate between them and is not
+// displayed. Each user has their own password and keycard which are attached to their
+// User object.
+
+User userlist[TOTAL_USERS];
 
 
-// The lists of whitelisted passwords / card IDs. Each corresponds to a user.
-// Need to improve to make object such that each user has their own of each.
-// Currently, any combination of valid password and cardID will work.
-User userlist[] = {
-  User(1, "1234", "B3 9D 7D 15"), 
-  User(2, "4321", "B1 9D 7D 15"), 
-  User(3, "1111", "B4 D4 C4 34"), 
-  User(4, "ABCD", "55 55 55 55")
-  };
 
 // RFID initialisation
 #define SS_PIN 53
 #define RST_PIN 5
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
 
+void userSetup() {
+  userlist[0] = User(-1, "", "");
+  userlist[0].SetID(0);
+  userlist[0].SetPassword("1234");
+  userlist[0].SetCardID("8C 72 EA 37");
+
+  userlist[1] = User(-1, "", "");
+  userlist[1].SetID(1);
+  userlist[1].SetPassword("ABCD");
+  userlist[1].SetCardID("B3 9D 7D 15");
+
+}
+
+
+// Serial and SPI setup
 void setup()
 {
-  Serial.begin(9600);         // Initiate a serial communication
-  input_password.reserve(32); // maximum input characters is 32, change if needed
+  userSetup();
+  Serial.begin(9600); // Initiate a serial communication
+  Serial.println("Initialising...");  
+  input_password.reserve(32); // The max size of password
   SPI.begin();                // Initiate  SPI bus
   mfrc522.PCD_Init();         // Initiate MFRC522
+  Serial.println("Done!");
   Serial.println("Awaiting card...");
   Serial.println();
 }
 
+// The unlock code. Only runs if both the card and password have been entered.
 void unlockSignal()
-{ // Write code to tell the user the door was unlocked.
+{
+  Serial.println("Two factor authenticaton complete, full access granted. Locking in 5 seconds...");
+  delay(5000);
+  Serial.println("Locking...");
+  current_user = -1;
+  phase1Verified = false;
+  phase2Verified = false;
+  Serial.println("Locked.");
 }
 
+// The main code loop, runs continuously.
 void loop()
 {
 
   if (phase1Verified && phase2Verified)
   {
-    Serial.println("Two factor authenticaton complete, full access granted.");
-    delay(3000);
     unlockSignal();
-    Serial.println("Locking...");
-    phase1Verified = false;
-    phase2Verified = false;
-    Serial.println("Locked.");
   }
 
+  // Runs the Keypad program every loop.
   Keypad();
   // Look for new cards
   if (!mfrc522.PICC_IsNewCardPresent())
@@ -98,7 +117,7 @@ void loop()
   content.toUpperCase();
   if (checkRFID(content.substring(1)) == true)
   {
-    Serial.println("Card authorised. Stage 1 complete.");
+    Serial.println("Card authorised.");
     Serial.println();
     phase1Verified = true;
     delay(1000);
@@ -112,27 +131,44 @@ void loop()
   }
 }
 
+// Little helper function, compares the card ID with one in the database.
 bool checkRFID(String to_check)
 {
-  for (int i = 0; i < 4; i++)
+  for (int i = 0; i < TOTAL_USERS; i++)
   {
-    if (to_check.c_str() == "test")
+    Serial.println("");
+    Serial.println("Checking ");
+    Serial.print(to_check);
+    Serial.print(" against ");
+    Serial.print(userlist[i].card_id);
+    Serial.println("");
+    if (to_check == userlist[i].card_id)
     {
+      current_user = userlist[i].id;
+      Serial.print("Current User: ");
+      Serial.println(current_user);
+      Serial.println("");
       return true;
-    }
-    else
-      return false;
+    }      
   }
+  return false;
 }
 
 void Keypad()
 {
   char key = keypad.getKey();
 
+  if (key && phase1Verified == false)
+  {
+    Serial.println("");
+    Serial.println("Please tap security card.");
+    return;
+  }
+
   if (key)
   {
     // Makes sure not to print a # when the user has finished inputting a password.
-    if (key != '#')
+    if (key != '#' && key != '*')
     {
       Serial.print('*');
     }
@@ -140,14 +176,18 @@ void Keypad()
     // Clears the password cache if the '*' is pressed.
     if (key == '*')
     {
+      Serial.println("");
+      Serial.println("Input cleared.");
+      Serial.println("");
       input_password = "";
     }
-    else if (key == '#')
+    
+    if (key == '#')
     {
       if (checkPassword(input_password) == true)
       {
         Serial.println("");
-        Serial.println("Keypad authorised. Stage 2 complete.");
+        Serial.println("Keypad authorised.");
         phase2Verified = true;
       }
       else
@@ -160,22 +200,29 @@ void Keypad()
 
       input_password = ""; // clear input password
     }
-    else
+    else if (key != '*')
     {
       input_password += key; // append new character to input password string
     }
   }
 }
 
+// If the password matches the current user's password,
+// and if so, grants access and resets the user (-1 indicative
+// of a null value.
 bool checkPassword(String x)
+
 {
-  // for (int i = 0; i < 4; i++)
-  // {
-  //   if (x.c_str() == userlist[i].getPassword())
-  //   {
-  //     return true;
-  //   }
-  //   else
-  //     return false;
-  // }
+  for (int i = 0; i < TOTAL_USERS; i++)
+  {
+    if (x == userlist[current_user].password)
+    {
+      Serial.println("Accepted.");
+      current_user = -1;
+      return true;
+    }      
+  }
+  Serial.println("Denied.");
+  current_user = -1;
+  return false;
 }
